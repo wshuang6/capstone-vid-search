e6795eb8134ba17742b32a8958b45fdd5e928f3e
wshuang6 <seraph617@gmail.com>
2017-03-09 14:26:14 -0500

    not a significant commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 http://api-public.guidebox.com/v2/search?api_key=7ceacb5ffc481ff8aed9719a341cb2bda30df935
 
-var SEARCHURL = "http://api-public.guidebox.com/v2/search?";
+const SEARCHURL = "http://api-public.guidebox.com/v2/search?";
 
 function getData(searchTerm, showOrMovie, callback){
   let searchQueryTerms = {
@@ -10,8 +10,14 @@ function getData(searchTerm, showOrMovie, callback){
   };
   $.getJSON(SEARCHURL, searchQueryTerms, callback);
 }
-function cB (results) {
-	console.log(results);
-}
 
-getData("stranger", "show", cB);
+//get stuff from api
+//correctly take information from what the API returns, then render html based on that
+//event listeners
+
+//html: basic DOM
+//samples of what some of the stuff inserted into DOM should look like (e.g. what classes it should have)
+//slightly more detailed HTML that has like, sample divs and texts
+
+//CSS: needs to be responsive, maybe flexbox or something
+//needs to look nice
